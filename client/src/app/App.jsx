1c97326a5b685d8b9e0294f00dbf89db33f66579
subhsiecoded <subhasish5702@gmail.com>
2025-06-05 01:04:58 +0530

    basic input

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,9 +1,9 @@
-// App.jsx
 import "./App.css";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LogIn from "features/auth/LogIn";
 import Register from "features/auth/Register";
 import Journal from "features/journal/components";
+import Redirect from "features/redirect/Redirect";
 import Loading from "features/loading/Loading";
 import useCurrentUser from "common/hooks/useCurrentUser";
 import useMode from "common/hooks/useMode";
@@ -14,17 +14,12 @@ import useSafety from "common/hooks/useSafety";
 import { updateAlarm } from "features/safety/safetySlice";
 import { useDispatch } from "react-redux";
 
-const today = new Date().toISOString().split("T")[0];
-
 const App = () => {
   const { hasCheckedUsername, username } = useCurrentUser();
   const { darkMode, toggleDarkMode } = useMode();
   const locked = useSafety();
   const dispatch = useDispatch();
-
   const handler = () => dispatch(updateAlarm());
-
-  if (!hasCheckedUsername) return <Loading />;
 
   return (
     <div
@@ -35,35 +30,25 @@ const App = () => {
     >
       <Error />
       <ToggleDarkModeButton {...{ darkMode, toggleDarkMode }} />
-
-      <Routes>
-        {/* Authenticated Routes */}
-        {username && (
+      {hasCheckedUsername ? (
+        username ? (
           <>
-            <Route path="/journal/:date" element={<Journal />} />
-            <Route
-              path="/"
-              element={<Navigate to={`/journal/${today}`} replace />}
-            />
-            <Route
-              path="*"
-              element={<Navigate to={`/journal/${today}`} replace />}
-            />
+            <Routes>
+              <Route path="/journal/:date" element={<Journal />} />
+              <Route path="*" element={<Redirect />} />
+            </Routes>
+            {locked && <Safety />}
           </>
-        )}
-
-        {/* Unauthenticated Routes */}
-        {!username && (
-          <>
+        ) : (
+          <Routes>
             <Route path="/login" element={<LogIn />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/" element={<Navigate to="/login" replace />} />
-            <Route path="*" element={<Navigate to="/login" replace />} />
-          </>
-        )}
-      </Routes>
-
-      {locked && <Safety />}
+            <Route path="*" element={<Redirect />} />
+          </Routes>
+        )
+      ) : (
+        <Loading />
+      )}
     </div>
   );
 };
